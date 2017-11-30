170586154e7790381bf7c4a4bba24d0779ac6053
Arcesilas <arcesilas@neutre.email>
2017-11-30 23:47:59 +0100

    Add url() method

--- src/SmartController.php
+++ src/SmartController.php
@@ -53,4 +53,31 @@ abstract class SmartController implements SmartControllerInterface
     {
         $this->params = $params;
     }
+
+    /**
+    * Return the URL corresponding to a given named route with specified parameters
+    * @method url
+    * @param  string $routeName The route name
+    * @param  array  $args      The parameters of the route (values for the placeholders)
+    * @return string            The valid URL, usable in links, redirects, etc.
+    */
+    public function url($routeName, $args = [])
+    {
+        // If no route of that name exists, do not bother
+        try {
+            $path = $this->router->getNamedRoute($routeName)->getPath();
+        } catch (\Exception $e) {
+            return $routeName;
+        }
+
+        // Find parameters in the route path
+        $regex = \FastRoute\RouteParser\std::VARIABLE_REGEX;
+
+        // Do not try to replace placeholders when values are not given
+        $return = preg_replace_callback("~$regex~x", function ($matches) use ($args) {
+            return $args[$matches[1]] ?? $matches[0];
+        }, $path);
+
+        return preg_replace('`(\[.+\])$`', '', $return);
+    }
 }
